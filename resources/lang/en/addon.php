03f14bacecf5e701d435e0525ef39307d6da3a64
Mike <sales@enlivenapp.com>
2015-03-07 18:18:59 -0500

    initial commit

---
+++ resources/lang/en/addon.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'name'        => 'Bootswatch Sandstone',
+    'description' => 'A (Bootstrap) Bootswatch Sandstone theme for PyroCMS v3.'
+];
---
+++ src/BootswatchSandstoneTheme.php
@@ -0,0 +1,16 @@
+<?php namespace Enlivenapp\BootswatchSandstoneTheme;
+
+use Anomaly\Streams\Platform\Addon\Theme\Theme;
+
+/**
+ * Class BootswatchSandstoneTheme
+ *
+ * @link          http://enlivenapp.com
+ * @author        Enliven Applications <sales@enlivenapp.com>
+ * @author        Michael Webber <mw@enlivenapp.com>
+ * @package       Enlivenapp/bootstrapThreeTheme
+ */
+class BootswatchSandstoneTheme extends Theme
+{
+
+}
